c51d3a2a6930912ac7de00ac8fec4a29b5b0e231
Michael Barela <michael.d.barela@gmail.com>
2022-08-12 20:10:15 -0700

    Initial commit with empty RF transmitter code.

---
+++ src/RF_Transmitter.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Wire.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "RF_Transmitter.h"
 
 // arduino functions
 void setup(void);
