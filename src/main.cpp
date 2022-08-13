ca6c4549528e89184ff1da38fd0fc1ecc98311a0
Michael Barela <michael.d.barela@gmail.com>
2022-08-12 17:02:52 -0700

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+// arduino functions
+void setup(void);
+void loop(void);
+
+// global variables
+uint8_t count;
+uint8_t prev_count;
+
+// main function
+int main(void){
+	init(); // required
+	setup();
+
+	// forever loop
+	for(;;){
+		loop();
+	}
+
+	return 0;
+}
+
+// Initial setup of program
+void setup(void){
+  Serial.begin(9600);
+  count = 0;
+  prev_count = 0;
+}
+
+// Main logic of the program
+void loop(void){
+	count = millis()/1000;
+	if((millis()%1000 == 0) && (count != prev_count)){
+		Serial.println(count);
+	}
+	prev_count = count;
+}
